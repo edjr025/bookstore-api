5e8db789e456bc8f8149799bc922e31f85c2f348
edjr025 <edvaldojr025@gmail.com>
2021-12-16 15:59:26 -0300

    POST para Livro

--- src/main/java/com/edvaldo/bookstore/service/LivroService.java
+++ src/main/java/com/edvaldo/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.edvaldo.bookstore.domain.Categoria;
 import com.edvaldo.bookstore.domain.Livro;
 import com.edvaldo.bookstore.repositories.LivroRepository;
 import com.edvaldo.bookstore.service.exceptions.ObjectNotFoundException;
@@ -43,5 +44,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
