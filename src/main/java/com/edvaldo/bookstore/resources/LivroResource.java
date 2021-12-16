000d7477dd09c9e20ff7fee1318fb7c662f981f7
edjr025 <edvaldojr025@gmail.com>
2021-12-16 16:07:33 -0300

    DELETE para livro

--- src/main/java/com/edvaldo/bookstore/resources/LivroResource.java
+++ src/main/java/com/edvaldo/bookstore/resources/LivroResource.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,7 +64,11 @@ public class LivroResource {
 			return ResponseEntity.created(uri).build();
 		}
 		
-		
-		
+		@DeleteMapping(value = "/{id}")
+		public ResponseEntity<Void> delete(@PathVariable Integer id){
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+			
 		
 }
